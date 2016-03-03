baf99d18c746b4dd2e585dd7bd9d7dd51cab96d4
Marina Igumnova <igum.marina@gmail.com>
2016-03-03 17:28:58 +0300

    Display items in js.console added HOMEWORK

--- public/app.js
+++ public/app.js
@@ -12,8 +12,27 @@ function something()
 function add_to_cart(id)
 {
 	var key = 'product_' + id;
+
 	var x = window.localStorage.getItem(key);
 	x = x * 1 + 1;
 
 	window.localStorage.setItem(key, x);
+console.log(key);
+	get_cart_size();
+}
+
+function get_cart_size()
+{
+	var localStorage=window.localStorage;
+	var sum = 0;
+
+
+	for(var i=0, len=localStorage.length; i<len; i++) {
+	    var key = localStorage.key(i);
+	    var value = localStorage[key];
+	    console.log(key + " => " + value);
+
+	    sum = sum + parseInt(value,10);
+	}
+	console.log('in cart ' + sum +' cakes');
 }
