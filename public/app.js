3a810a82bbcc9298e0db536e453bee7ad202881c
Marina Igumnova <igum.marina@gmail.com>
2016-03-19 14:04:51 +0300

    Clear localStorage on cancel, update button

--- public/app.js
+++ public/app.js
@@ -87,6 +87,10 @@ function update_cart_page()
 function cancel_order()
 
 {
-	alert('aaa');
+	window.localStorage.clear();
+
+		update_orders_input();
+		update_orders_button();
+		
 	return false;
 }
